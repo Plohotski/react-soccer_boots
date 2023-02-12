68c35bc336bbce954e06aab48908b15b3bc637ec
Plohotski <https://github.com/Plohotski>
2023-02-12 22:18:36 +0300

    feat: start of development

--- boots/src/App.js
+++ boots/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="header">
+        <section className="header-left">
+          <svg className="header-left__logo"/>
+          <div className="header-left__info">
+            <h3 className="header-left__info-title">Movement Rhythm</h3>
+            <p className="header-left__info-subtitle">Store for the best sneakers</p>
+          </div>
+        </section>
+        <section className="header-right">
+          <div className="header-right__buy">
+            <svg className="header-right__buy-logo"/>
+            <span className="header-right__buy-cost">1205 $</span>
+          </div>
+          <div className="header-right__user">
+            <svg className="header-right__user-logo" />
+          </div>
+        </section>
       </header>
+      <main>
+        <section className="content">
+          <h1>All Sneakers</h1>
+          .....
+        </section>
+      </main>
     </div>
   );
 }
